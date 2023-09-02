c766d1bdcb5a05b3a876e368aa41ede63cf26aec
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 02:54:58 +0200

    Update api.py

--- api.py
+++ api.py
@@ -28,13 +28,18 @@ def loaded():
     return "ICI API…"
 
 
-
 @app.post('/predict/')
 async def scoring(data: dict):
-    data_df = pd.DataFrame.from_dict(data, orient='index').transpose()
-    applicant_score = model.predict_proba(data_df)[0][1] * 100 
-    response_data = {"status": "ok", "score": applicant_score}
-    return JSONResponse(content=response_data)
+    response_text = "Le score est : 42"  
+    return JSONResponse(content=response_text)
+
+
+# @app.post('/predict/')
+# async def scoring(data: dict):
+#     data_df = pd.DataFrame.from_dict(data, orient='index').transpose()
+#     applicant_score = model.predict_proba(data_df)[0][1] * 100 
+#     response_data = {"status": "ok", "score": applicant_score}
+#     return JSONResponse(content=response_data)
 
 # @app.post('/predict/')
 # async def scoring(data: dict):
