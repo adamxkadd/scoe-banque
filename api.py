ac6cb337394d483c963ed8588205b9a99950086f
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 20:36:02 +0200

    Update api.py

--- api.py
+++ api.py
@@ -111,7 +111,10 @@ async def interpretation(data: dict):
     return {"summary_plot": encoded_img}
 
 
+# if __name__ == "__main__":
+#     import uvicorn
+#     # uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
+#     uvicorn.run(app, host="https://scoe-banque.streamlit.app/")
+
 if __name__ == "__main__":
-    import uvicorn
-    # uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
-    uvicorn.run(app, host="https://scoe-banque.streamlit.app/")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
